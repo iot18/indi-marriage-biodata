0e10b810e642a1d0354a7c748de006f8c130684c
ramah <ramahrudayb@gmail.com>
2025-05-16 00:18:48 -0500

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,34 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
 // core styles are required for all packages
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
+import Script from "next/script";
 
 // other css files are required only if
 // you are using components from the corresponding package
-import '@mantine/dates/styles.css';
+import "@mantine/dates/styles.css";
 // import '@mantine/dropzone/styles.css';
 // import '@mantine/code-highlight/styles.css';
 // ...
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HKNDMP39FQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+           gtag('config', 'G-HKNDMP39FQ');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default Application;
